78e5bc76128aaf9f04a081796bb208b1831adf6d
msstoev <msstoev@gmail.com>
2018-03-03 08:15:38 +0000

    Adding the files

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+source /etc/default/metron
+
+sudo $METRON_HOME/bin/zk_load_configs.sh -z $ZOOKEEPER -m PULL -o ${METRON_HOME}/config/zookeeper/ -f 
+
+if [ ! -f ${METRON_HOME}/config/zookeeper/profiler.json ] 
+then 
+  echo '{ "profiles" : [] }' | sudo tee ${METRON_HOME}/config/zookeeper/profiler.json
+  sudo $METRON_HOME/bin/zk_load_configs.sh -z $ZOOKEEPER -m PUSH -i ${METRON_HOME}/config/zookeeper/
+fi
+
+$METRON_HOME/bin/zk_load_configs.sh -z $ZOOKEEPER -m PATCH -c PROFILER -pf profiler_patch.json
